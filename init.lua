7864cfbf5c07cba4064542897f8d3e253005f2ed
rchojn <rafalchojnacki0502@gmail.com>
2025-09-20 15:08:17 +0200

    relative lines numbers

--- init.lua
+++ init.lua
@@ -133,7 +133,7 @@ vim.api.nvim_create_autocmd('UIEnter', {
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
